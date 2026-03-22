787440164c01acfd48a646b1700a11cf051e02ce
riccardob46 <riccardo.bertti1998@gmail.com>
2026-03-22 12:38:13 +0100

    Tipi di dati _ex

---
+++ tipididati.py
@@ -0,0 +1,18 @@
+x= 7
+y= 3
+somma= x + y
+print(somma)
+
+prezzo= 19.99
+quantità= 3
+totale= prezzo * quantità
+print(totale)
+
+numero=10
+print(numero > 5) #true
+print(numero < 5) #false
+
+a= 5
+b= 2.5
+somma= a + b
+print(somma)
