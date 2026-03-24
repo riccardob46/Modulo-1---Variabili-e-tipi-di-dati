d89b963a87355280b611c57fbff76b5b0697b090
riccardob46 <riccardo.bertti1998@gmail.com>
2026-03-24 18:45:37 +0100

    esercizio liste

---
+++ esercizio_liste.py
@@ -0,0 +1,6 @@
+#es. fine videolezione: crea una lista con 5 numeri, aggiungi un numero alla fine, sostituisci il tezo numero, stampa la lista finale
+numeri=[1,2,3,4,5]
+numeri.append(6)
+numeri.remove(3)
+numeri.insert(2, 10)
+print(numeri)
