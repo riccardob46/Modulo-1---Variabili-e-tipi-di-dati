4b4a3fc7d8ee9efd1d313f0ced0cca9ba7a9a89b
riccardob46 <riccardo.bertti1998@gmail.com>
2026-03-24 19:19:11 +0100

    esercizio tuple

---
+++ esercizio_tuple.py
@@ -0,0 +1,5 @@
+#esercizio fine video: creare una tupla con 3 colori, stampa il primo e l'ultimo, conta quante volte compare un colore
+colori=("viola","viola","verde")
+print(colori[0])
+print(colori[-1])
+print(colori.count("viola"))
